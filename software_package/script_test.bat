b69ace91a56387f6161dde0ea41e2360c57a23b2
haixiao990 <haixiao990@aliyun.com>
2018-05-20 20:37:49 +0800

    update software package

---
+++ software_package/script_test.bat
@@ -0,0 +1,11 @@
+echo unweightednetworks
+.\exefiles\CUCorMatHr.exe E:/data/HCPdata E:/data/G0001/G0001.nii 0.2 n r p 0.6
+.\exefiles\CUBFW_Lp.exe E:/data/HCPdata/unweighted 1 gnl
+.\exefiles\Cp.exe E:/data/HCPdata/unweighted 1 gnk
+.\exefiles\SmallWorldProperty.exe E:/data/HCPdata/unweighted 
+.\exefiles\CUmodularity.exe E:/data/HCPdata/unweighted 1
+.\exefiles\PC_CPU.exe E:/data/HCPdata/unweighted 
+.\exefiles\Degree.exe E:/data/HCPdata/unweighted
+.\exefiles\CUBC.exe E:/data/HCPdata/unweighted
+.\exefiles\CUEC.exe E:/data/HCPdata/unweighted
+.\exefiles\ConvertNII.exe E:/data/HCPdata/unweighted E:/data/G0001/G0001.nii 0.2
